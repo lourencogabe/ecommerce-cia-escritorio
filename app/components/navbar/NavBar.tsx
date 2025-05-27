1f2c8d7f0a7c8d9a577e9943559813537c9d5b0a
lourencogabe <gabriels211@hotmail.com>
2025-05-27 17:18:16 -0300

    feat: stylize  navbar

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,5 +1,19 @@
+import Container from "../Container";
+
 const NavBar = () => {
-    return  <div>NavBar</div> ;
+    return (
+        <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
+            <div className="py-4 border-b-[1px">
+                <Container>
+                    <div className="flex items-center justify-between gap-3 md-gap">Home</div>
+                    <div>Cadeiras</div>
+                    <div>Moveis</div>
+                    <div>Serviços</div>
+                    <div>Contato</div>
+                </Container>
+            </div>
+        </div>
+        ) 
 }
  
 export default NavBar;
